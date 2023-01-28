ffbb0af027d0a97465badcd6efd39cffceca1a28
fmathisc <mathischerbonnier1207@gmail.com>
2023-01-28 18:37:34 +0100

    Home page design buttons, colors + image logo + Widgets + pubspec dependancies and assets

---
+++ lib/widgets/ButtonCircleTextWidget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ButtonCircleWithTextWidget extends StatelessWidget {
+  String text;
+  ButtonCircleWithTextWidget(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 55.0,
+      height: 55.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(5.0, 5.0),
+            blurRadius: 3.0,
+            spreadRadius: 3,
+          ),
+        ],
+      ),
+      child: FlatButton(
+        onPressed: () {
+          // Code for button press here
+        },
+        child: Center(
+          child: Text(
+            "${text}",
+            style: TextStyle(
+                color: Color(0xFF267DAE),
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+                fontSize: 20),
+          ),
+        ),
+      ),
+    )
+
+    ;
+  }
+}
